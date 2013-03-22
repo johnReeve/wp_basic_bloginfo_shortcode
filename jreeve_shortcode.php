8d08c19ec2e107ead29e35d54b99d11edd8dae1a
John Reeve <John.reeve.media@gmail.com>
2013-03-22 13:47:35 -0500

    first commit

---
+++ jreeve_shortcode.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @package bloginfo_ShortCode
+ * @version 1.6
+ */
+/*
+Plugin Name: Bloginfo Short Code
+Plugin URI: http://johnrreeve.com	
+Description: Very Simple plug to make it so that I cna put the bloginfo into a short code
+Author: John Reeve
+Version: 1.0
+Author URI: http://johnrreeve.com
+*/
+
+function jreeve_bloginfo_shortcode( $atts ) {
+   extract(shortcode_atts(array(
+       'key' => '',
+   ), $atts));
+   return get_bloginfo($key);
+}
+
+add_shortcode('bloginfo', 'jreeve_bloginfo_shortcode');
